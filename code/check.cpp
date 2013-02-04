39a241c5700e1b279d38f3ec037ddb26c50284c6
stigersh <stigersh@gmail.com>
2013-02-04 22:12:50 +0200

    added a nice template

--- code/check.cpp
+++ code/check.cpp
@@ -3,3 +3,8 @@ template<
     class Traits = std::char_traits<CharT>, 
     class Allocator = std::allocator<CharT>
 > class basic_string;
+
+template< class CharT, class Traits, class Alloc >
+    basic_string<CharT,Traits,Alloc>
+        operator+( const basic_string<CharT,Traits,Alloc>& lhs,
+                   const CharT* rhs );
